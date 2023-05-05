2f20dc2a2c914c75df6e51fa4e0f42343372a0dd
Heraud Sandra <sandra.heraud@ynov.com>
2023-05-05 08:09:21 +0000

    Card start

--- 6-qui-prend/client/src/App.tsx
+++ 6-qui-prend/client/src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
-import Home from './pages/Home'
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './config/router'
